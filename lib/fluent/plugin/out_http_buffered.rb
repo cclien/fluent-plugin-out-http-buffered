5eae44dc8888bdbdce087f8f1a3fc9403594a69c
CC Lien <cclien@gmail.com>
2015-12-11 09:17:15 +0800

    http_method option split large chunk into multiple HTTP requests (max # of event per HTTP request is configurable) change HTTP body flavor from [tag,time,record] to record only

--- lib/fluent/plugin/out_http_buffered.rb
+++ lib/fluent/plugin/out_http_buffered.rb
@@ -2,6 +2,10 @@
 
 module Fluent
   # Main Output plugin class
+
+  class HTTPretryException < StandardError
+  end
+
   class HttpBufferedOutput < Fluent::TimeSlicedOutput
     Fluent::Plugin.register_output('http_buffered', self)
 
@@ -14,14 +18,23 @@ module Fluent
     # Endpoint URL ex. localhost.local/api/
     config_param :endpoint_url, :string
 
+    # http verb
+    config_param :http_method, :string, :default => :post
+
+    # max # of events per HTTP request 
+    config_param :http_event_limit, :integer
+
+    # max retries if HTTP request fail
+    config_param :http_fail_retry, :integer, default: 10
+
     # statuses under which to retry
     config_param :http_retry_statuses, :string, default: ''
 
     # read timeout for the http call
-    config_param :http_read_timeout, :float, default: 2.0
+    config_param :http_read_timeout, :float, default: 300.0
 
     # open timeout for the http call
-    config_param :http_open_timeout, :float, default: 2.0
+    config_param :http_open_timeout, :float, default: 60.0
 
     # Addtional HTTP Header
     config_param :additional_headers, :string, :default=> nil
@@ -45,14 +58,13 @@ module Fluent
 
       @statuses = [] if @statuses.nil?
 
-      @http = Net::HTTP.new(@uri.host, @uri.port)
-      @http.use_ssl = (@uri.scheme == "https")
-      @http.read_timeout = @http_read_timeout
-      @http.open_timeout = @http_open_timeout
-
       # Convert string to Hash (Header name => string)
       if @additional_headers
         @additional_headers = Hash[@additional_headers.split(",").map{ |f| f.split("=",2)}]
+      end
+
+      if not [:get,:post,:put,:delete].include? @http_method.intern
+        raise Fluent::ConfigError, 'http method invalid'
       end
 
     end
@@ -64,7 +76,7 @@ module Fluent
     def shutdown
       super
       begin
-        @http.finish
+        #@http.finish
       rescue
       end
     end
@@ -73,52 +85,81 @@ module Fluent
       [tag, time, record].to_msgpack
     end
 
-    def write(chunk)
-      data = []
-      chunk.msgpack_each do |(tag, time, record)|
-        data << [tag, time, record]
-      end
-
+    def http_write(data)
+      http = Net::HTTP.new(@uri.host, @uri.port)
+      http.use_ssl = (@uri.scheme == "https")
+      http.read_timeout = @http_read_timeout
+      http.open_timeout = @http_open_timeout
       request = create_request(data)
 
+      retry_count = 0
       begin
-        response = @http.start do |http|
-          request = create_request(data)
+        retry_count = retry_count+1
+        if retry_count > @http_fail_retry
+          fail "Retry too much times, abort"
+        end
+        response = http.start do |http|
           http.request request
         end
 
         if @statuses.include? response.code.to_i
           # Raise an exception so that fluent retries
-          fail "Server returned bad status: #{response.code}"
+          #fail "Server returned bad status: #{response.code}"
+          raise HTTPretryException, "Server returned bad status: #{response.code}"
         end
+      rescue HTTPretryException => e
+        $log.warn "Got HTTP client error #{e.message}, retry #{retry_count}"
+        retry
       rescue IOError, EOFError, SystemCallError => e
         # server didn't respond
         $log.warn "Net::HTTP.#{request.method.capitalize} raises exception: #{e.class}, '#{e.message}'"
       ensure
         begin
-          @http.finish
+          http.finish
         rescue
         end
       end
     end
 
-    protected
-
-      def create_request(data)
-        request = Net::HTTP::Post.new(@uri.request_uri)
-
-        # Headers
-        request['Content-Type'] = 'application/json'
-        if @additional_headers
-          @additional_headers.each{|k,v|
-            request[k] = v
-          }
+    def write(chunk)
+      buf = []
+      threads = []
+      data = []
+      chunk.msgpack_each do |(tag, time, record)|
+        buf << record
+        if buf.length >= @http_event_limit
+          data << buf
+          small_chunk_index = data.length-1 
+          threads << Thread.new { http_write(data[small_chunk_index]) }
+          buf = []
         end
-
-        # Body
-        request.body = JSON.dump(data)
-
-        request
       end
-  end
-end
+
+      if buf.length > 0
+        threads << Thread.new { http_write(buf) }
+      end
+      threads.each { |thr| thr.join }
+
+      $log.debug "Wrote chunk size #{chunk.size} with #{threads.length} threads"
+
+    end
+
+    protected
+    def create_request(data)
+      request = Net::HTTP.const_get(@http_method.to_s.capitalize).new(@uri.request_uri)
+
+      # Headers
+      request['Content-Type'] = 'application/json'
+      if @additional_headers
+        @additional_headers.each{|k,v|
+          request[k] = v
+        }
+      end
+
+      # Body
+      request.body = JSON.dump(data)
+
+      request
+    end
+  end # class
+end # module
